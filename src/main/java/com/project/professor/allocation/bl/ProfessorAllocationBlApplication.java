7653bad03b37bbb8fe6a496cd1df9bc58354ebd0
Bruno Léo Silva <brunoleosilva@hotmail.com>
2024-02-24 14:29:13 -0300

    add project

---
+++ src/main/java/com/project/professor/allocation/bl/ProfessorAllocationBlApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.bl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationBlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationBlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/alocation/bl/ProfessorAlocationBlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.alocation.bl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAlocationBlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
