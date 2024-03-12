49636d17e880d80f9626b588f2d7820a64622533
Bruno Léo Silva <brunoleosilva@hotmail.com>
2024-03-11 22:17:11 -0300

    update course class test

--- src/test/java/com/project/professor/allocation/repository/CourseRepositoryTest.java
+++ src/test/java/com/project/professor/allocation/repository/CourseRepositoryTest.java
@@ -1,5 +1,9 @@
 package com.project.professor.allocation.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
@@ -7,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
 
+import com.project.professor.allocation.entity.Course;
 import com.project.professor.allocation.repository.CourseRepository;
 
 @DataJpaTest
@@ -16,5 +21,59 @@ import com.project.professor.allocation.repository.CourseRepository;
 public class CourseRepositoryTest {
 
 	@Autowired
-	CourseRepository courseRepository;
+	public CourseRepository courseRepository;
+	
+	@Test
+	public void createTest() {
+		Course course = new Course();
+		course.setName("Arquitetura de Software Back-End");
+		
+		Course cTest = courseRepository.save(course);
+		
+		System.out.println(cTest);
+	}
+	
+	@Test
+	public void updateTest() {
+		Course course = new Course();
+		course.setId(1L);
+		course.setName("Arquitetura de Software Front-End");
+		
+		Course cTest = courseRepository.save(course);
+		
+		System.out.println(cTest);
+	}
+	
+	@Test
+	public void readTest() {
+		List<Course> dp = courseRepository.findAll();
+		
+		System.out.println(dp);
+	}
+	
+	@Test
+	public void readOneTest() {
+		Optional<Course> course = courseRepository.findById(2L);
+		
+		if (!course.isEmpty()) {
+			System.out.println(course);
+		}
+		else {
+			System.out.println("Course not found");
+		}
+	}
+	
+	
+	@Test
+	public void deleteTest() {
+		Optional<Course> course = courseRepository.findById(2L);
+		
+		if (!course.isEmpty()) {
+			courseRepository.deleteById(course.get().getId());
+			System.out.println("Deleted course");
+		}
+		else {
+			System.out.println("Course not found");
+		}
+	}
 }
